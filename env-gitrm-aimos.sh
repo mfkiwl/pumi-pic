956f46297ed47cdc28799315d47ae275eec884ba
Gopan Perumpilly <gopan.p@gmail.com>
2020-09-18 22:34:58 -0400

    updated config scripts

--- env-gitrm-aimos.sh
+++ env-gitrm-aimos.sh
@@ -2,17 +2,16 @@ module use /gpfs/u/software/dcs-spack-install/v0133gccSpectrum/lmod/linux-rhel7-
 module load spectrum-mpi/10.3-doq6u5y
 module load gcc/7.4.0/1
 module load cmake/3.15.4-mnqjvz6
-module load netcdf-cxx4/4.3.1-7lwefeg
+#module load netcdf-cxx4/4.3.1-7lwefeg
+module load netcdf-cxx4/4.3.1-4es6a7y
 
 build=build-dcs-gcc74
-export root=/gpfs/u/home/MPFS/MPFSgppr/barn-shared/gopan/libraries
-export kk=$root/${build}-kokkos/install
-export oh=$root/${build}-omegah/install
-
 export pp=/gpfs/u/barn/MPFS/shared/gopan/gitrmwork/build-dcs-gcc74-pumipic-gitrm/install
 cuda=/usr/local/cuda-10.1
 export PATH=$cuda/bin:$PATH
 export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
-export CMAKE_PREFIX_PATH=$kk:$oh:$pp:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=$pp:$CMAKE_PREFIX_PATH
+export root=/gpfs/u/barn/MPFS/shared/gopan/libraries
 export OMPI_CXX=$root/kokkos/bin/nvcc_wrapper
+export OIMPI_CC=gcc
 
--- envBlockadeCuda.sh
+++ envBlockadeCuda.sh
@@ -3,13 +3,13 @@ module load mpich
 module load cmake
 module load netcdf
 module load cuda/10.1-k3n6svu
+
 ncxx=/lore/gopan/install/build-netcdfcxx431/install
 #libconf=/lore/gopan/install/build-libconfig-1.7.2/install
 #export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${libconf}/lib64/pkgconfig
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${ncxx}/lib64/pkgconfig
 pp=/lore/gopan/install/build-pumipic-cuda-rhel7-blockade-gitrm/install
-engpar=/lore/gopan/install/build-engpar-rhel7/install
-export CMAKE_PREFIX_PATH=${engpar}:${pp}:${ncxx}:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=${pp}:${ncxx}:$CMAKE_PREFIX_PATH
 #:${libconf}
 kksrc=/lore/gopan/install/kokkos
 export MPICH_CXX=${kksrc}/bin/nvcc_wrapper
